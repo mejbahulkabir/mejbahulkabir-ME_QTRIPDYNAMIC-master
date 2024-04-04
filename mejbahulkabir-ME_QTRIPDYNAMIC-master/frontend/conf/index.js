4318863b6fef4ebff181ed13cf5e23db2fc1fa3a
mejbahulkabir <mejbahulkabir@gmail.com>
2024-04-04 10:52:38 +0530

    link change

--- mejbahulkabir-ME_QTRIPDYNAMIC-master/frontend/conf/index.js
+++ mejbahulkabir-ME_QTRIPDYNAMIC-master/frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://13.233.150.184:8081" };
+const config = { backendEndpoint: "https://mejbahulkabir-me-qtripdynamic-master-6.onrender.com" };
 
 export default config;
